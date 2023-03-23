aaa38171e733894fb3b70999627305d2c98e00d9
SubhamSingh9667 <sisubham99@gmail.com>
2023-03-23 19:50:58 +0530

    commit first

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import MovieProvider from "./context/Movie.context";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <MovieProvider>
+        <App />
+      </MovieProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
